98779a4ffc10e119c4461ea06bba8fdaa0c9614b
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-28 00:10:49 -0500

    on progress....

--- sources/apps/native/global-scripts/load-window.js
+++ sources/apps/native/global-scripts/load-window.js
@@ -1,7 +1,21 @@
-function loadWindow(htmlPath, width, heigth) {
+const hbs = require('handlebars');
+
+function loadWindow(htmlPath, width, height, appName) {
+    let divDesktop = document.getElementById('desktop-for-windows');
     let extname = path.extname(htmlPath);
     if (!extname || extname != '.html') {
         throw 'the extname file is not html';
     };
-    console.log(htmlPath, width, heigth);
+    htmlPath = path.join(globalInfoSystemObject.ROOT_ROUTE, htmlPath);
+    if (!fs.existsSync(htmlPath)) {
+        throw 'the file noesn\'t exists';
+    };
+    const principalWindow = fs.readFileSync(path.join(globalInfoSystemObject.ROOT_ROUTE, '/apps/native/windows/principal.hbs')).toString();
+    divDesktop.insertAdjacentHTML('afterbegin', hbs.compile(principalWindow)({
+        body: fs.readFileSync(htmlPath).toString(),
+        width,
+        height,
+        appName
+    }));
+
 }
