ae24373808d78419321534128b9342a252f0ed55
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-30 17:02:57 -0500

    now you can put a little icon on the top of the title

--- sources/apps/FileExplorer/index.js
+++ sources/apps/FileExplorer/index.js
@@ -1,5 +1,5 @@
 module.exports = class fileExplorer {
     static main(){
-        loadWindow('/apps/fileExplorer/index.html', 200, 300, 'file explorer');
+        loadWindow('/apps/fileExplorer/index.html', 200, 300, 'file explorer', '/apps/fileExplorer/images/icon-file-explorer.png');
     }
 };
--- sources/apps/native/global-scripts/load-window.js
+++ sources/apps/native/global-scripts/load-window.js
@@ -1,6 +1,6 @@
 const hbs = require('handlebars');
 
-function loadWindow(htmlPath, width, height, appName) {
+function loadWindow(htmlPath, width, height, appName, iconPath) {
     let divDesktop = document.getElementById('desktop-for-windows');
     let extname = path.extname(htmlPath);
     if (!extname || extname != '.html') {
@@ -11,11 +11,13 @@ function loadWindow(htmlPath, width, height, appName) {
         throw 'the file noesn\'t exists';
     };
     const principalWindow = fs.readFileSync(path.join(globalInfoSystemObject.ROOT_ROUTE, '/apps/native/windows/principal.hbs')).toString();
+    if(iconPath) iconPath = path.join(globalInfoSystemObject.ROOT_ROUTE, iconPath);
     divDesktop.insertAdjacentHTML('afterbegin', hbs.compile(principalWindow)({
         body: fs.readFileSync(htmlPath).toString(),
         width,
         height,
-        appName
+        appName,
+        iconPath
     }));
 
 }
