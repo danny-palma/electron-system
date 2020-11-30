f8ccadad73838fa82cee033e86e404d3a65adf40
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-30 16:19:17 -0500

    new registry file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { join } from "path";
-import { existsSync, writeFileSync,  } from "fs";
+import { existsSync, writeFileSync } from "fs";
 // verification of registry
 if (!existsSync(join(__dirname, '../sources/registry/initial-config.json'))) {
     writeFileSync(join(__dirname, '../sources/registry/initial-config.json'), JSON.stringify({
         'full-screen': false
+    }));
+};
+if (!existsSync(join(__dirname, '../sources/registry/installed-apps.json'))) {
+    writeFileSync(join(__dirname, '../sources/registry/installed-apps.json'), JSON.stringify({
+        apps: [
+            {
+                name: 'File Explorer',
+                icon_route: '/apps/FileExplorer/images/icon-file-explorer.png',
+                main_route: '/apps/FileExplorer/index.js',
+                id: '001'
+            },
+            {
+                name: 'Internet',
+                icon_route: '/apps/navegador/images/icon-explorer.png',
+                main_route: '/apps/navegador/main.js',
+                id: '002'
+            }
+        ]
     }));
 };
 import Main from './app';
