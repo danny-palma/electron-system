ce28ff91e48d5ecc8dd34536e50acb7cd3c035a5
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-12-11 15:07:09 -0500

    improved function move window

--- sources/system/global-scripts/move-window.js
+++ sources/system/global-scripts/move-window.js
@@ -1,8 +1,14 @@
-function moveWindow(that = document.getElementById("asd"), event1) {
-    console.log(event1)
-    that.addEventListener("mousemove", (event) => {
+function moveWindow(that, event1) {
+    that.onmouseup = () => {
+        that.onmousemove = null;
+    };
+    that.onmouseleave = () => {
+        that.onmouseup = null;
+        that.onmousemove = null;
+    }
+    that.onmousemove = (event) => {
         event.preventDefault();
         that.parentElement.style.top = `${event.y - 10}px`;
         that.parentElement.style.left = `${event.x - 100}px`;
-   });
+    };
 }
