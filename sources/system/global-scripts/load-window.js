970930ec3ca621525e1f042523337741eff83969
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-12-16 09:48:39 -0500

    now the load window accepts an object & changed the name of the function

--- sources/system/global-scripts/load-window.js
+++ sources/system/global-scripts/load-window.js
@@ -1,36 +1,47 @@
 const hbs = require('handlebars');
 
-function loadWindowWithPath(htmlPath, width, height, appName, iconPath) {
-    let divDesktop = document.getElementById('desktop-for-windows');
-    let extname = path.extname(htmlPath);
-    if (!extname || extname != '.html') {
-        throw 'the extname file is not html';
-    };
-    htmlPath = path.join(globalInfoSystemObject.ROOT_ROUTE, htmlPath);
-    if (!fs.existsSync(htmlPath)) {
-        throw 'the file noesn\'t exists';
-    };
-    const principalWindow = fs.readFileSync(path.join(globalInfoSystemObject.ROOT_ROUTE, '/system/windows/principal.hbs')).toString();
-    if(iconPath) iconPath = path.join(globalInfoSystemObject.ROOT_ROUTE, iconPath);
-    divDesktop.insertAdjacentHTML('afterbegin', hbs.compile(principalWindow)({
-        body: fs.readFileSync(htmlPath).toString(),
-        width,
-        height,
-        appName,
-        iconPath
-    }));
-    return document.getElementById(`${appName}-window`);
+// little interface
+let interfaceArgs = {
+    type: 'string' || 'file',
+    width: 0,
+    height: 0,
+    title: '',
+    icon: '',
+    htmlPath: '',
+    stringBody: ''
 }
-function loadWindowWithString(string, width, height, appName, iconPath) {
+/**
+ * @param {interfaceArgs} args
+ */
+function showWindow(args) {
     let divDesktop = document.getElementById('desktop-for-windows');
-    const principalWindow = fs.readFileSync(path.join(globalInfoSystemObject.ROOT_ROUTE, '/system/windows/principal.hbs')).toString();
-    if(iconPath) iconPath = path.join(globalInfoSystemObject.ROOT_ROUTE, iconPath);
+    if (args.type == 'file') {
+        let extname = path.extname(args.htmlPath);
+        if (!extname || extname != '.html') {
+            throw 'the extname file is not html';
+        };
+        args.htmlPath = path.join(globalObject.ROOT_ROUTE, args.htmlPath);
+        if (!fs.existsSync(args.htmlPath)) {
+            throw 'the file noesn\'t exists';
+        };
+        args.body = fs.readFileSync(args.htmlPath).toString();
+    } else if (args.type == 'string') {
+        args.body = args.stringBody;
+    } else {
+        throw 'the type was not provided or is invalid'
+    }
+    const principalWindow = fs.readFileSync(path.join(globalObject.ROOT_ROUTE, '/system/windows/principal.hbs')).toString();
+    if (args.icon) args.icon = path.join(globalObject.ROOT_ROUTE, args.icon);
+    globalObject.OPEN_WINDOWS.push({
+        id: `${globalObject.OPEN_WINDOWS.length + 1}-window`,
+    })
     divDesktop.insertAdjacentHTML('afterbegin', hbs.compile(principalWindow)({
-        body: string,
-        width,
-        height,
-        appName,
-        iconPath
+        body: args.body,
+        width: args.width || 720,
+        height: args.height || 480,
+        appName: args.title || 'app',
+        iconPath: args.icon,
+        id: globalObject.OPEN_WINDOWS.length
     }));
-    return document.getElementById(`${appName}-window`);
+    return document.getElementById(`${globalObject.OPEN_WINDOWS.length}-window`);
 }
