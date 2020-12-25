e569dc9adf2a3ac05ad11e5934b8e174819eeeec
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-12-24 21:11:12 -0500

    now is secure to execute the function main

--- sources/apps/FileExplorer/index.js
+++ sources/apps/FileExplorer/index.js
@@ -1,23 +1,25 @@
- module.exports = class fileExplorer {
-    static main(...args) {
-        function fileExplorerUpdateFolder(path = '/') {
-            let folders = [1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-            fs.readdirSync(`${globalObject.ROOT_ROUTE}${path}`).map((value) => {
-                folders.push(value);
-            })
-            return hbs.compile(fs.readFileSync(`${__dirname}/index.hbs`).toString())({
-                folders
+module.exports = class fileExplorer {
+    static get main() {
+        return (...args) => {
+            function fileExplorerUpdateFolder(path = '/') {
+                let folders = [1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+                fs.readdirSync(`${globalObject.ROOT_ROUTE}${path}`).map((value) => {
+                    folders.push(value);
+                })
+                return hbs.compile(fs.readFileSync(`${__dirname}/index.hbs`).toString())({
+                    folders
+                });
+            };
+            showWindow({
+                type: 'string',
+                stringBody: fileExplorerUpdateFolder(),
+                width: 720,
+                heigth: 480,
+                title: 'file explorer',
+                icon: '/apps/fileExplorer/images/icon-file-explorer.png'
             });
-        };
-        console.log(showWindow({
-            type: 'string',
-            stringBody: fileExplorerUpdateFolder(),
-            width: 720,
-            heigth: 480,
-            title: 'file explorer',
-            icon: '/apps/fileExplorer/images/icon-file-explorer.png'
-        }));
-        require('./updateexplorer');
+            require('./updateexplorer');
+        }
     }
     static onclose() {
         console.log("app is closed");
@@ -25,7 +27,7 @@
     static onminimize() {
         console.log("app is minimized");
     }
-    static onmaximized(){
+    static onmaximized() {
         console.log("app is maximized");
-    } 
+    }
 };
