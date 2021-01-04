c9cbe8664c5482f2294d32aac99cc7823fcba530
holasebas <danielpalma2003@hotmail.com>
2021-01-04 12:29:42 -0500

    new global variables

---
+++ sources/system/global-scripts/globalVariables.js
@@ -0,0 +1,28 @@
+let globalVariables = new class globalVariables {
+    constructor() {
+        let fileReaded = JSON.parse(fs.readFileSync(path.join(globalObject.ROOT_ROUTE, 'registry/variables.json'))?.toString());
+        this.systemroot = globalObject.ROOT_ROUTE
+        Object.keys(fileReaded).map((value) => {
+            typeof fileReaded[value] == "string" ? fileReaded[value] = fileReaded[value].replace(/%systemroot%/g, this.systemroot) : null;
+            this[value.toLowerCase()] = fileReaded[value];
+        });
+        Object.freeze(this);
+    }
+    get registerVariable() {
+        return function regiterVariable(key, value) {
+            if (!key) throw new Error("the key is undefined")
+            if (typeof key != "string") throw new Error(`the type of the key can only are typeof string recived ${typeof key}`);
+            if (typeof value == "bigint" ||
+                typeof value == "symbol" ||
+                typeof value == "function" ||
+                value === undefined ||
+                value === null) throw new Error(`the type of key can be only are typeof string, boolean, array, object recived ${typeof value}`);
+            let fileReaded = JSON.parse(fs.readFileSync(path.join(globalObject.ROOT_ROUTE, 'registry/variables.json'))?.toString());
+            fileReaded[key.toLowerCase()] = value;
+            fs.writeFileSync(path.join(globalObject.ROOT_ROUTE, 'registry/variables.json'), JSON.stringify(fileReaded));
+            return true;
+        };
+    }
+}
+
+module.exports = globalVariables
